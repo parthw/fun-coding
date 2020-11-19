28d24617f178831b516298d773bcf1fc6241ff6a
parthw <wparth777@gmail.com>
2020-11-20 00:51:57 +0530

    d6: stuck with viper unmarshal

---
+++ golang/d6-cobra-viper-continued/main.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2020 parth
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package main
+
+import (
+	"fmt"
+
+	"example.com/cobra-viper/cmd"
+	"github.com/spf13/viper"
+)
+
+// Myconfig example
+type Myconfig struct {
+	username string `mapstructure:"username"`
+}
+
+func main() {
+	cmd.Execute()
+	fmt.Println("I can print this ", viper.AllSettings())
+	var mc Myconfig
+	if err := viper.Unmarshal(&mc); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(mc)
+}
